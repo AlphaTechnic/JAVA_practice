d592f1df97a857023e29e82a017c6a7aa102dda2
AlphaTechnic <alphatechnicist@gmail.com>
2021-06-26 23:34:35 +0900

    Learn about variables in JAVA

---
+++ JAVA_practice2/src/org/opentutorials/javatutorials/stringpractice/StringDemo.java
@@ -0,0 +1,8 @@
+package org.opentutorials.javatutorials.stringpractice;
+
+public class StringDemo {
+    public static void main(String[] args){
+        String first = "coding";
+        System.out.println(first);
+    }
+}
---
+++ JAVA_practice2/src/org/opentutorials/javatutorials/variables/IntvalDoubleval.java
@@ -0,0 +1,14 @@
+package org.opentutorials.javatutorials.variables;
+
+public class IntvalDoubleval {
+    public static void main(String[] args){
+        int a;
+        a = 1;
+        System.out.println(a + 1);
+        System.out.println(a + 1.2);
+
+        double b;
+        b = 1.1;
+        System.out.println(b + 1.2);
+    }
+}
