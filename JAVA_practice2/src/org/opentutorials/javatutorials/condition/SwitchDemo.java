6d0786f350e20fe36d1bf1456c4c4878989be946
AlphaTechnic <alphatechnicist@gmail.com>
2021-06-28 21:31:30 +0900

    Practice switch statement

---
+++ JAVA_practice2/src/org/opentutorials/javatutorials/condition/SwitchDemo.java
@@ -0,0 +1,21 @@
+package org.opentutorials.javatutorials.condition;
+
+public class SwitchDemo {
+    public static void main(String[] args){
+        System.out.println("switch(2)");
+        switch(13){
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            default:
+                System.out.println("default block");
+                break;
+        }
+    }
+}
